ff37ab77f3d190570e7fb87ddbfa04928dc465e8
Mingyi Liu <liumy@hit.edu.cn>
2024-03-08 16:53:31 +0800

    v0.2 separate functions

--- src/HIndex.java
+++ src/HIndex.java
@@ -12,28 +12,37 @@ public class HIndex {
             citations[i] = Integer.parseInt(strs[i]);
         }
 
+        int hindex = hindex(citations);
+
+        System.out.println("The h-index is: " + hindex);
+    }
+
+    public static int hindex(int[] citations) {
         // bubble sorting
-        int number = strs.length;
-        for (int i = 0; i < number - 1; i++) {
-            for (int j = 0; j < number - i - 1; j++) {
-                if (citations[j] < citations[j+1] ) {
-                    int temp = citations[j+1];
-                    citations[j+1] = citations[j];
-                    citations[j] = temp;
-                }
-            }
-        }
+        sort(citations);
 
         // calculate h-index
         int hindex = 0;
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < citations.length; i++) {
             if (citations[i] >= i + 1) {
                 hindex = i + 1;
             } else {
                 break;
             }
         }
+        return hindex;
+    }
 
-        System.out.println("The h-index is: " + hindex);
+    private static void sort(int[] array) {
+        int number = array.length;
+        for (int i = 0; i < number - 1; i++) {
+            for (int j = 0; j < number - i - 1; j++) {
+                if (array[j] < array[j+1] ) {
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
