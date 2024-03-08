c2aa6786e49e84cd7224f5cbe7f90232c8038893
Mingyi Liu <liumy@hit.edu.cn>
2024-03-08 17:11:25 +0800

    v0.4 avoid input string containing illegal characters

--- src/HIndex.java
+++ src/HIndex.java
@@ -6,16 +6,31 @@ public class HIndex {
         int[] citations = new int[100];
         String[] strs;
         System.out.println("Please input the citation numbers:");
-        String line = scanner.nextLine();
 
-        while (line.isEmpty()) {
-            System.out.println("Input is empty, please re-input:");
-            line = scanner.nextLine();
-        }
+        // loop, until user inputs legal string
+        while (true) {
+            String line = scanner.nextLine();
+            // if the input is empty.
+            while (line.isEmpty()) {
+                System.out.println("Input is empty, please re-input:");
+                line = scanner.nextLine();
+            }
 
-        strs = line.split(",");
-        for (int i = 0; i < strs.length; i++) {
-            citations[i] = Integer.parseInt(strs[i]);
+            // check if each part is non-negative integer
+            boolean legalNumbers = true;
+            strs = line.split(",");
+            for (int i = 0; i < strs.length; i++) {
+                // if not, stop checking others and let user re-input.
+                if (! strs[i].matches("[0-9]+")) {
+                    System.out.println(strs[i] + " is illegal, please re-input:");
+                    legalNumbers = false;
+                    break;
+                }
+                citations[i] = Integer.parseInt(strs[i]);
+            }
+            if (legalNumbers) {
+                break;
+            }
         }
 
         int hindex = hindex(citations);
